9847bbf0a964181804aa26ad76178f433f8a472f
sshivanshg <145559709+sshivanshg@users.noreply.github.com>
2024-09-18 16:15:01 +0530

    Update link.jsx (url updated)

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://trimrr.in/${link}`}
+            href={`https://shorten-url-blush.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://trimrr.in/{link}
+           https://shorten-url-blush.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://trimrr.in/${link}`)
+                navigator.clipboard.writeText(`https://shorten-url-blush.vercel.app/${link}`)
               }
             >
               <Copy />
